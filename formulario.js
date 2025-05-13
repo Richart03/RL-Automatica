c2e6d42c14abd16175116e7225127d2d7f4f7b7c
Richart03 <richardmorenoalonso@gmail.com>
2025-05-13 12:03:26 -0700

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,11 +1,5 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-    // Validación adicional si es necesaria
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const servicio = document.getElementById('servicio').value;
-    const mensaje = document.getElementById('mensaje').value;
-    
     e.preventDefault();
     alert('Formulario enviado correctamente')
     this.submit();
-    console.log('Formulario enviado:', { nombre, email, servicio, mensaje }); }); 
+     }); 
